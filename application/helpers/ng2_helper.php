b457596e0a470865c677556939dca036cd56bde8
Evgeni Vasilev <aquilax@gmail.com>
2011-03-31 12:54:59 +0300

    Added some simple gamewide help functions;

---
+++ application/helpers/ng2_helper.php
@@ -0,0 +1,26 @@
+<?php
+
+//Increments a value with optional upper limit;
+function ng2_inc($current, $val, $max = FALSE){
+  $sum = $current + $val;  
+  if ($max !==FALSE){
+    return ($sum > $max)?$max:$sum;
+  }
+  return $sum;
+}
+
+//Decrements a value with optional lower limit;
+function ng2_inc($current, $val, $min = FALSE){
+  $sum = $current - $val;  
+  if ($min !==FALSE){
+    return ($sum < $min)?$min:$sum;
+  }
+  return $sum;
+}
+
+//Returns random number between min and max;
+function ng2_rand($min, $max){
+  return mt_rand($min, $max);
+}
+
+?>
