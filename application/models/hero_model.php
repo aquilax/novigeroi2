5ee6c00470e9298230333b0dafb8e7dfa89f191d
Evgeni Vasilev <aquilax@gmail.com>
2011-03-31 13:24:20 +0300

    Added createHero, listHeroes functions

--- application/models/hero_model.php
+++ application/models/hero_model.php
@@ -7,6 +7,36 @@ class Hero_model extends Base_char_Model{
 
   private $table_name = 'hero'; //hero's table;
 
+  //List all heroes for user
+  public function listHeroes($user_id){
+    $this->db->where('user_id', $user_id);
+    $this->db->order_by('created');
+    $query = $this->db->get($this->table_name);
+    return $query->result_array();
+  }
+
+  //Create new hero;
+  public function createHero($user_id, $name, $class_id){
+    //Will keep all template heroes in the table with owner user_id 0;
+    $this->where('user_id', 0);
+    $this->where('class', $class_id);
+    $this->db->limit(1);
+    $query = $this->db->get($this->table_name);
+    $data = $query->row_array();
+    if (!$data){
+      //Class Template not found;
+      return FALSE;  
+    }
+    
+    unset($data[$this->key]);
+    $data['name'] = $name;
+    $data['user_id'] = $user_id; //override the user_id
+    $this->db->set($data); //set the template data;
+    $this->db->set('created', 'CURRENT_TIMESTAMP', FALSE);  //set the current timestamp for creation;
+    $this->db->insert($this->table_name);
+    return  $this->db->insert_id();
+  }
+
 }
 
 ?>
