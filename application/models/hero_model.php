9b4a688313613215783519182123cecc1259af68
Evgeni Vasilev <aquilax@gmail.com>
2011-03-31 11:48:34 +0300

    Hero Model should use Base_char_model;

--- application/models/hero_model.php
+++ application/models/hero_model.php
@@ -1,6 +1,11 @@
 <?php
 
-class Hero_model extends AQX_Model{
+//this is ugly but I don't know ho to have multiple inheritance in CI;
+require_once APPPATH.'/models/base_char_model.php';
+
+class Hero_model extends Base_char_Model{
+
+  private $table_name = 'hero'; //hero's table;
 
 }
 
