fe9cbe3e17f89c3e0d8a7507fb09fc0292d0c5cc
Evgeni Vasilev <aquilax@gmail.com>
2011-04-05 19:42:43 +0300

    Added some info to hero

--- application/controllers/v1/hero.php
+++ application/controllers/v1/hero.php
@@ -5,12 +5,17 @@ class Hero extends AQX_InGame_Controller{
   function __construct(){
     parent::__construct();
     $this->load->model('hero_model');
-    $this->hero_model->load($this->hero_id);
+    $this->hero_model->load(array('id' => $this->hero_id));
   }
 
-  function getInfo(){
+  function info(){
     $this->addData('name', $this->hero_model->get('name'));
-    $this->addData('hp', $this->hero_model->get('hp'));
+    $this->addData('hp', $this->hero_model->get('hp', 0));
+    $this->addData('hp_max', $this->hero_model->get('hp_max', 0));
+    $this->addData('mp', $this->hero_model->get('mp', 0));
+    $this->addData('mp', $this->hero_model->get('mp_max', 0));
+    $this->addData('x', $this->hero_model->get('x', -1));
+    $this->addData('y', $this->hero_model->get('y', -1));
     $this->render();
   }
 
@@ -29,12 +34,12 @@ class Hero extends AQX_InGame_Controller{
     $this->render();
   }
 
-  function use(){
+  function use_item(){
     $this->setStatus(501, 'Not Implemented');
     $this->render();
   }
 
-  function drop(){
+  function drop_item(){
     $this->setStatus(501, 'Not Implemented');
     $this->render();
   }
