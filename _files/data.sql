bdf2b3bd2e16dd2db682f11df0072269ea61311e
Evgeni Vasilev <aquilax@gmail.com>
2011-04-06 05:59:41 +0300

    Added basic item tables;

--- _files/data.sql
+++ _files/data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.56, for debian-linux-gnu (i486)
+-- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: ng2
 -- ------------------------------------------------------
--- Server version	5.1.56-1
+-- Server version	5.1.49-1ubuntu8.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -21,12 +21,26 @@
 
 LOCK TABLES `hero` WRITE;
 /*!40000 ALTER TABLE `hero` DISABLE KEYS */;
-<<<<<<< HEAD
 INSERT INTO `hero` VALUES (1,0,1,'Варварин',1,1,'2011-03-31 15:40:36','0000-00-00 00:00:00','home',NULL,1,0,0,'{}'),(2,1,1,'123',1,1,'2011-04-01 04:15:19','0000-00-00 00:00:00','town',1,1,0,0,'{}'),(3,1,1,'цуцо',1,1,'2011-04-03 04:11:27','0000-00-00 00:00:00','home',NULL,1,0,0,'{}');
-=======
-INSERT INTO `hero` VALUES (1,0,1,'Варварин',1,1,'2011-03-31 15:40:36','0000-00-00 00:00:00','home',NULL,1,0,0,'{}'),(2,1,1,'123',1,1,'2011-04-01 04:15:19','0000-00-00 00:00:00','town',1,1,0,0,'{}');
->>>>>>> 46b8c1de048c13ca0ea6a6eec17f0faf081c6f2a
 /*!40000 ALTER TABLE `hero` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `item_type`
+--
+
+LOCK TABLES `item_type` WRITE;
+/*!40000 ALTER TABLE `item_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -35,12 +49,17 @@ UNLOCK TABLES;
 
 LOCK TABLES `place` WRITE;
 /*!40000 ALTER TABLE `place` DISABLE KEYS */;
-<<<<<<< HEAD
 INSERT INTO `place` VALUES (1,1,1,'ЦУМ','Всичко за всеки','2011-04-02 05:50:42',''),(2,1,2,'ВМА','Лекуваме','2011-04-04 02:39:43','');
-=======
-INSERT INTO `place` VALUES (1,1,1,'ЦУМ','Всичко за всеки','2011-04-02 05:50:42',''),(2,1,2,'ВМА','Лекуваме','2011-04-04 07:12:40','');
->>>>>>> 46b8c1de048c13ca0ea6a6eec17f0faf081c6f2a
 /*!40000 ALTER TABLE `place` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `place_inventory`
+--
+
+LOCK TABLES `place_inventory` WRITE;
+/*!40000 ALTER TABLE `place_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `place_inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,11 +68,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `place_type` WRITE;
 /*!40000 ALTER TABLE `place_type` DISABLE KEYS */;
-<<<<<<< HEAD
-INSERT INTO `place_type` VALUES (1,'Магазин','store','2011-04-02 05:50:10'),(2,'Болница','hospital','2011-04-04 02:38:56');
-=======
 INSERT INTO `place_type` VALUES (1,'Магазин','store','2011-04-02 05:50:10'),(2,'Лечебница','place/hospital','2011-04-04 07:13:36');
->>>>>>> 46b8c1de048c13ca0ea6a6eec17f0faf081c6f2a
 /*!40000 ALTER TABLE `place_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,8 +91,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-<<<<<<< HEAD
--- Dump completed on 2011-04-04  6:29:19
-=======
--- Dump completed on 2011-04-04 12:16:26
->>>>>>> 46b8c1de048c13ca0ea6a6eec17f0faf081c6f2a
+-- Dump completed on 2011-04-06  5:59:19
--- application/controllers/v1/store.php
+++ application/controllers/v1/store.php
@@ -3,13 +3,22 @@
 class Store extends AQX_InTown_Controller{
   
   private $store_type = 3;
+  private $store_id = -1;
 
   function __construct(){
     parent::__construct();
     $this->load->model('store_model');
+    $store_id = (int)$this->uri->segment(4);
+    $id = $this->store_model->load(array('id' => $store_id, 
+      'place_type_id' => $this->store_type));
+    if (!$id){
+      $this->guard();
+    }
   }
 
   function list_items(){
+    $data = $this->store_model->getItems($store_id);
+    $this->addData('data', $data);
     $this->setStatus(501, 'Not Implemented');
     $this->render();     
   }
